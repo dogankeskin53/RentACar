3bea1578a318ffd2e5e37aa62fcfdc435971dc50
dogankeskin53 <dogankeskin53@gmail.com>
2021-04-17 22:45:27 +0300

    Katmanların ve klasörlerin oluşturulması

---
+++ RentAcarED/DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    /// <summary>
+    /// Veritabanında product ile yapacağımız işlemleri ayarlayacamız yer
+    /// Crud işlemleri kısaca.
+    /// </summary>
+    public  interface IProductDal
+    {
+        /// <summary>
+        /// Entities katmanı referans olarak alındı.
+        /// Buradaki mantık, ürünleri listele ? nereden entity katmanından
+        /// o zaman erişebilmek için referans eklemek zorundayız.
+        /// interface methodları default olarak public'tir.
+        /// </summary>
+        /// <returns></returns>
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ RentAcarED/Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    /// <summary>
+    /// IEntity implement eden class bir veribanı tablosudur.
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+}
